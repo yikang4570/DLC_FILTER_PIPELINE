675ebf35e9a0b5c8347ec88087e8ca27b753a09e
Shawn Pavey <shawn.pavey@yahoo.com>
2025-07-11 16:09:02 -0500

    Calculator top velocity has now been validated

--- dlcfilterpipeline/calculator_top_velocity.py
+++ dlcfilterpipeline/calculator_top_velocity.py
@@ -4,6 +4,8 @@ import numpy as np
 from scipy.io import loadmat
 import readyplot as rp
 import pandas as pd
+from tkinter import filedialog
+import os
 
 class calculator_top_velocity:
     def __init__(self, data_file_path):
@@ -22,11 +24,11 @@ class calculator_top_velocity:
             'Left Fore Duty Factor',
             'Right Fore Duty Factor',
             'Fore Duty Factor',
-            'ST Imbalance Fore',
+            'DF Imbalance Fore',
             'Left Hind Duty Factor',
             'Right Hind Duty Factor',
             'Hind Duty Factor',
-            'ST Imbalance Hind',
+            'DF Imbalance Hind',
             'Fore Limb Temporal Symmetry',
             'Hind Limb Temporal Symmetry',
             'Fore Step Width',
@@ -249,8 +251,110 @@ class calculator_top_velocity:
         axes[1].get_legend().set_visible(False)
         sub.save()
 
+def import_manually_excluded_filenames(input_folder):
+    manually_excluded_filenames = []
+    excluded_filename = "manually_determined_failures.txt"
+    in_input = os.path.join(input_folder, excluded_filename)
+    in_subplots = os.path.join(input_folder, "SUBPLOTS", excluded_filename)
+
+    if os.path.isfile(in_input):
+        print(f"File manually_determined_failures.txt found in input folder: {in_input}")
+        filepath = in_input
+    elif os.path.isfile(in_subplots):
+        print(f"File manually_determined_failures.txt found in SUBPLOTS folder: {in_subplots}")
+        filepath = in_subplots
+    else:
+        print("No such file: manually_determined_failures.txt, check paths or congratulations on no failures!")
+        return []
+
+    with open(filepath, 'r') as f:
+        for raw_line in f:
+            path = raw_line.strip()
+            base, _ = os.path.splitext(os.path.basename(path).split('_SUB')[0])
+            filename = base + "_DATA.mat"
+            manually_excluded_filenames.append(filename)
+            print(f"Manually excluded file: {filename}")
+
+    return manually_excluded_filenames
+
+def get_excluded_filenames(input_folder,files):
+    files_to_exclude = []
+    manually_excluded_filenames = import_manually_excluded_filenames(input_folder)
+    files_to_exclude.extend(manually_excluded_filenames)
+
+    for file in files:
+        if 'calibration' in file:
+            files_to_exclude.append(file)
+            print(f"Excluded file: {file}")
+
+    return files_to_exclude
+
+def get_pixel_resolution(input_folder):
+    meters_per_pixel = None
+    return meters_per_pixel
+
+def create_master_excel(input_folder,excel_files):
+    col_names = [
+        'File',
+        'Left Fore Duty Factor',
+        'Right Fore Duty Factor',
+        'Fore Duty Factor',
+        'DF Imbalance Fore',
+        'Left Hind Duty Factor',
+        'Right Hind Duty Factor',
+        'Hind Duty Factor',
+        'DF Imbalance Hind',
+        'Fore Limb Temporal Symmetry',
+        'Hind Limb Temporal Symmetry',
+        'Fore Step Width (m)',
+        'Hind Step Width (m)',
+        'Fore Stride Length (m)',
+        'Hind Stride Length (m)',
+        'Spatial Symmetry Fore',
+        'Spatial Symmetry Hind',
+        'Top Nose Velocity (m/frame)',
+        'Top Body Velocity (m/frame)',
+        'Bottom meters/pixel'
+    ]
+
+    output_rows = []
+    meters_per_pixel = get_pixel_resolution(input_folder)
+
+    for file in excel_files:
+        output_row = [file.split('.')[0]]
+        temp_df = pd.read_excel(os.path.join(input_folder,file))
+        average_data_over_cycles = temp_df.mask(temp_df == 0).mean(axis=0)
+
+        columns_to_convert_to_metric = [
+            'Fore Step Width (m)',
+            'Hind Step Width (m)',
+            'Fore Stride Length (m)',
+            'Hind Stride Length (m)',
+            'Top Nose Velocity (m/frame)',
+            'Top Body Velocity (m/frame)']
+
+        output_row.extend(average_data_over_cycles)
+        output_row.append(meters_per_pixel)
+        output_rows.append(pd.Series(dict(zip(col_names,output_row))))
+
+    df = pd.DataFrame(output_rows, columns=col_names)
+    df.to_excel(os.path.join(input_folder,"MASTER_GAIT.xlsx"), index=False)
+
+
 if __name__ == '__main__':
-    filename = '/Users/paveyboys/Desktop/TOP_VELOCITY/20250121_4353_01_DATA.mat'
-    calculator_top_velocity(filename)
+    manual_folder = filedialog.askdirectory(title="Select Folder")
+    files = sorted([f for f in os.listdir(manual_folder) if f.endswith('.mat')])
+    files_to_exclude = get_excluded_filenames(manual_folder, files)
+    output_excel_files = []
+
+    for filename in files:
+        if filename not in files_to_exclude:
+            print("Calculating parameters for: " + str(filename))
+            calculator_top_velocity(os.path.join(manual_folder, filename))
+            output_excel_files.append(filename.split('_DATA.mat')[0]+'.xlsx')
+        else:
+            print(f"Skipping {filename}.")
+
+    create_master_excel(manual_folder,output_excel_files)
 
 
